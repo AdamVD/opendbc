6987efcd80c8c1ffb7b964c695175e29f15e8058
MVL <manuel@alumni.virginia.edu>
2026-03-29 15:04:41 -0400

    Update minimum steer speed logic in carstate.py
    Change minimum steer speed calculation for Honda vehicles to a fixed value for testing.

--- opendbc/car/honda/carstate.py
+++ opendbc/car/honda/carstate.py
@@ -100,7 +100,7 @@ class CarState(CarStateBase):
     ret.steerFaultPermanent = steer_status not in ("NORMAL", "NO_TORQUE_ALERT_1", "NO_TORQUE_ALERT_2", "LOW_SPEED_LOCKOUT", "TMP_FAULT")
     if self.CP.carFingerprint in HONDA_BOSCH_ALT_RADAR:
       # TODO: See if this logic works for all other Honda
-      min_steer_speed = max(CarControllerParams.STEER_GLOBAL_MIN_SPEED, self.CP.minSteerSpeed)
+      min_steer_speed = 3 * CV.MPH_TO_MS # test, was: max(CarControllerParams.STEER_GLOBAL_MIN_SPEED, self.CP.minSteerSpeed)
       expected_low_speed_lockout = steer_status == "LOW_SPEED_LOCKOUT" and ret.vEgo < min_steer_speed
       ret.steerFaultTemporary = steer_status != "NORMAL" and not expected_low_speed_lockout
     else:
